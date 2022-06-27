2d005605d4c0a743ce3afebf014a0eff5cb641b3
IIeaf <yaprak_1x1@hotmail.com>
2022-06-27 10:10:42 +0200

    Assignment3 fixed hopefully

--- assignment3/assignment3.sh
+++ assignment3/assignment3.sh
@@ -4,35 +4,34 @@
 #        Assignment3         #
 ##############################
 
-# Job name
-#SBATCH --job-name=minimap2
+#SBATCH --partition=assemblix
 
-# Allow all notifications about the job
+#SBATCH --job-name=minimap2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=y.yigit@st.hanze.nl
 #SBATCH --account=yyigit
 
-# The partition on which the job shall run
-#SBATCH --partition=assemblix
-
-# N+1 Threads for Minimap2
 #SBATCH --nodes=1
-# From 1 to 16
-#SBATCH --array=1-16
-# --Memory per node 
-#SBATCH --mem-per-cpu=1500MB
-
-# How long the job is allowed to run in real time, 
-# formatted as d-hh:mm:ss
-#SBATCH --time=0-02:00:00
+# Run the job for max 4 hours
+#SBATCH --time=0-04:00:00
+# For minimap 2 N+1 threads
+#SBATCH --cpus-per-task=17
+#SBATCH --mem-per-cpu=4500MB
 
 # Source conda so minimap2 is loaded into the environment
 source /commons/conda/conda_load.sh
 
-# File locations
-index=$( realpath "$0"  ) && /data/dataprocessing/MinIONData/all_bacteria.fna "$index"
-data=$( realpath "$0"  ) && /data/dataprocessing/MinIONData/all.fq "$data"
+# Data I tested on
+index=../Data/reference.fa
+data=../Data/sequence.fa
 
-# The output goes to /dev/null
-/usr/bin/time -o timings.txt --append -f "${n}\t%e" minimap2 -N "$n"+1 -a $index $data > "/dev/null" 2> log.txt
+# Actual file locations
+#index=/data/dataprocessing/MinIONData/all_bacteria.fna
+#data=/data/dataprocessing/MinIONData/all.fq
 
+# For loop to generate time for each CPU
+# -t is the amount of CPU's 
+# -a will map the reference and sequence to SAM format
+for ((n = 1; n <= 16; n++)); do
+    /usr/bin/time -o timings.txt --append -f "${n}\t%e" minimap2 -t $n+1 -a $index $data > "/dev/null" 2> log.txt
+done
